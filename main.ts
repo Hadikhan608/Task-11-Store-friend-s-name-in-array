945b7bb099c6a528f3f00978b5f0fe849603debd
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-05-05 23:04:20 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,7 @@
+//Task # 11, Names:store the names of a few of your friends in an array called Names.
+//print each person's names by accessing each element in the list one at a time. 
+
+let Names:string [] = ["Kumail","Shahid","Tahami"];
+console.log(Names[0]);
+console.log(Names[1]);
+console.log(Names[2]);
